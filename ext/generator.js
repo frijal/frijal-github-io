913305eb6a9ca67913cba932dd4fd8c191a6e494
Fakhrul Rijal <5477182+frijal@users.noreply.github.com>
2025-09-28 15:36:53 +0800

    Update generator.js

--- ext/generator.js
+++ ext/generator.js
@@ -105,8 +105,7 @@ fs.writeFileSync(jsonOut, JSON.stringify(grouped, null, 2), "utf8");
 
 // Simpan sitemap.xml
 const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${xmlUrls.join("\n")}
 </urlset>`;
 fs.writeFileSync(xmlOut, xmlContent, "utf8");
