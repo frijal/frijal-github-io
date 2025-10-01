3d864b162a156b88e1473de26a64d694091180aa
Fakhrul Rijal <5477182+frijal@users.noreply.github.com>
2025-10-01 10:33:51 +0800

    Delete ext/marquee-init.js

--- ext/marquee-init.js
+++
@@ -1,6 +0,0 @@
-import { initMarqueeDynamic } from './marquee.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initMarqueeDynamic('marquee-bottom', 1, 60000); 
-  // 1 = speed, 60000 = refresh setiap 60 detik
-});
