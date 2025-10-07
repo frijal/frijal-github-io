67d20c8a61a74abd075a523ce688e7c5cffc1986
frijal <5477182+frijal@users.noreply.github.com>
2025-10-07 20:55:16 +0800

    hapus ip info

--- artikel/sisipkanbaris.sh
+++ artikel/sisipkanbaris.sh
@@ -5,19 +5,19 @@ for file in *.html; do
   echo "🔍 Memproses $file ..."
 
   # Tambah div iposbrowser setelah </h1>
-  if grep -qi 'document.addEventListener('DOMContentLoaded', () => {' "$file"; then
-    echo "ℹ️ Sudah ada document.addEventListener('DOMContentLoaded', () => { di $file"
-  else
-    sed -i '/<script>/a document.addEventListener('DOMContentLoaded', () => {' "$file"
-    echo "✅ document.addEventListener('DOMContentLoaded', () => { di $file"
-  fi
+ # if grep -qi 'document.addEventListener('DOMContentLoaded', () => {' "$file"; then
+  #  echo "ℹ️ Sudah ada document.addEventListener('DOMContentLoaded', () => { di $file"
+  # else
+   #  sed -i '/<script>/a document.addEventListener('DOMContentLoaded', () => {' "$file"
+   # echo "✅ document.addEventListener('DOMContentLoaded', () => { di $file"
+  #fi
 
   ### Tambah related marquee + script sebelum </body>
- # if grep -qi 'defer src="/ext/ipos' "$file"; then
- #   echo "ℹ️ Sudah ada skrip related di $file"
+ #if grep -qi 'defer src="/ext/ipos' "$file"; then
+  #  echo "ℹ️ Sudah ada skrip related di $file"
  # else
- #   sed -i '/<\/body>/i <script defer src="/ext/iposbrowser.js"></script>' "$file"
- #   echo "✅ Ditambahkan skrip iposbrowser di $file"
+    sed -i '/<\/script>/i });' "$file"
+    echo "✅ Ditambahkan skrip iposbrowser di $file"
  # fi
 
 done
