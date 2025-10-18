802bae73d8cd3244e1c633fc5b34c1c9cd13a2a3
frijal <5477182+frijal@users.noreply.github.com>
2025-10-18 14:41:53 +0800

    update sabtu siang 10-18

--- artikel/replace-meta-images.sh
+++ artikel/replace-meta-images.sh
@@ -22,7 +22,7 @@ find . -type f -name "*.html" | while read -r FILE; do
   s|"https://frijal\.pages\.dev/assets/(.*?)\.png"|"https://frijal.pages.dev/img/\1.webp"|g;  
   s|"https://frijal\.pages\.dev/assets/(.*?)\.jpg"|"https://frijal.pages.dev/img/\1.webp"|g; 
    
-  s|https://i.ibb.co/L5hY6bX/xfce-infographic-image.jpg|https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgEwXRdmiz34VXRa3Xc5MpggCjskgA78kpzCI1aVgn6anxe6k9-9qIZnYJLJr6Afqs2H9EUb4Y33zw2M0rhe0UDChnUZ229867ZjBKO55RDcQQB-MApYsE0FUckvPRmFVZqaK43-N1s64aY4navaGE1oPeQU2e3m1v3Zy8TJ4r4-axyv4nNo5VPRo3cVDY/s1600/317-wallpaper.png|g;    
+  s|html-vs-header.webp|head-vs-header.webp|g;    
   ' "$FILE"
 
   AFTER_HASH=$(sha1sum "$FILE" | awk '{print $1}')
