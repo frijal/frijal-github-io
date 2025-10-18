1a0f0eda18b0febcb5a4793c03e17bfc2d3e38f6
frijal <5477182+frijal@users.noreply.github.com>
2025-10-18 11:02:19 +0800

    update sabtu siang 10-18

--- artikel/replace-meta-images.sh
+++ artikel/replace-meta-images.sh
@@ -19,6 +19,8 @@ find . -type f -name "*.html" | while read -r FILE; do
   s|<meta property="og:image" content="https://frijal\.pages\.dev/artikel/(.*?)\.jpg">|<meta property="og:image" content="https://frijal.pages.dev/artikel/\1.webp">|g;
   s|<meta itemprop="image" content="https://frijal\.pages\.dev/artikel/(.*?)\.jpg">|<meta itemprop="image" content="https://frijal.pages.dev/img/\1.webp">|g;
   s|<meta itemprop="image" content="https://frijal\.pages\.dev/artikel/(.*?)\.webp">|<meta itemprop="image" content="https://frijal.pages.dev/img/\1.webp">|g;
+  s|<meta name="twitter:image" content="https://frijal\.pages\.dev/artikel/(.*?)\.webp">|<meta name="twitter:image" content="https://frijal.pages.dev/img/\1.webp">|g;
+  s|<meta property="og:image" content="https://frijal\.pages\.dev/artikel/(.*?)\.webp">|<meta property="og:image" content="https://frijal.pages.dev/img/\1.webp">|g;  
   ' "$FILE"
 
   AFTER_HASH=$(sha1sum "$FILE" | awk '{print $1}')
